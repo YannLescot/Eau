1f30a4b95212b5d6ac89eb804489d1fa9acf51b9
AyoubTahar <tahar.ayoub@gmail.com>
2023-03-10 16:20:35 +0100

    Pb undifined

--- eau06.js
+++ eau06.js
@@ -1,9 +1,23 @@
 let string = process.argv
 let stringMaj =''
-function MajusculeSurDeux (arg){
+let majusculeSurDeux =''
+function RecupString (arg){
     for(let i = 2 ; i < arg.length ; i++){
+        if (stringMaj !== ''){
+            stringMaj += " ";
+        }
         stringMaj += arg[i]
     }
 }
-MajusculeSurDeux(string)
-console.log(stringMaj)
+
+function MajusculeSurDeux (arg) {
+    for(let i = 0 , j = i+1; i === arg.length , j <= arg.length; i+=2,j+=2 ){
+        
+        majusculeSurDeux += arg[i].toUpperCase() + arg[j]
+      
+    }
+}
+RecupString(string)
+MajusculeSurDeux(stringMaj)
+console.log(stringMaj[5])
+console.log(majusculeSurDeux)
