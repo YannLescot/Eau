47c0b0b86392d4c5c9b56676e13783f1a3e2ea92
AyoubTahar <tahar.ayoub@gmail.com>
2023-03-16 09:05:35 +0100

    fini

--- eau11.js
+++ eau11.js
@@ -1,36 +1,41 @@
 let string = process.argv
 let tableau =[]
 let resultat = []
-
 function RecupNb (arg){
 for (let i = 2 ; i< arg.length ; i++){
+    if (isNaN(arg[i])) {
+        console.log(`error`)
+        process.exit() // arrête le script avec le code d'erreur 1
+    }
     tableau[i-2] = arg[i]
 }
 }
 
 function Calcul (tab){
+    let k = 0;
     for (let i = 0 ; i < tab.length -1 ; i++){
-        for (let j = i+1 ; j < tab.length  ; j++){
-            if(resultat !== "") {
-                resultat += " ";
-              }//Pour avoir un résultat toujours positif
-              if((tab[i] - tab[j]) < 0){
-                resultat += (tab[i] - tab[j])* -1
-
-              }
-              else{
-                resultat += tab[i] - tab[j]
-
-              }
-            
+        for (let j = i+1; j < tab.length ; j++){
+            //Pour avoir un résultat toujours positif
+            if((tab[i] - tab[j]) < 0){
+                resultat[k] = (tab[i] - tab[j])* -1
+            }
+            else{
+                resultat[k] = (tab[i] - tab[j])
+            }
+            k++;
         }
     }
 }
 
 function Mini (tab){
-    let mini =[]
-    
+    let min = tab[0]
+    for (let i = 1 ; i< tab.length ; i++){
+        if (tab[i] < min){
+            min = tab[i]
+        }
+    }
+    console.log(min)
 }
 RecupNb(string)
 Calcul(tableau)
-console.log(resultat)
+Mini(resultat)
