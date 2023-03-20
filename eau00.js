b40593998d61db2b9efa073dfdc485e56b682bcd
Yann Lescot <60198564+YannLescot@users.noreply.github.com>
2023-03-20 11:56:11 +0100

    Exemple paramètres
    Ajout de paramètre à la fonction + return au lieu de console

--- eau00.js
+++ eau00.js
@@ -1,16 +1,17 @@
 let combinaison = ''
-function Combinaison3Chiffres (){
+
+function Combinaison3Chiffres (cbn){
     for (let i = 0; i < 10; i++) {
         for (let j = i + 1; j < 10; j++) {
           for (let k = j + 1; k < 10; k++) {
-                if (combinaison !== ''){
-                    combinaison += ",";
+                if (cbn !== ''){
+                    cbn += ",";
                 }
-              combinaison +=(`${i}${j}${k}`);
+              cbn +=(`${i}${j}${k}`);
         }
       }
 }
-console.log(combinaison)
+return(cbn)
 }
 
-Combinaison3Chiffres()
+console.log(Combinaison3Chiffres(combinaison))
